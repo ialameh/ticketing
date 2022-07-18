663365c84f01ef332db2c27fef832362a82d36c5
issam Alameh <issam@alameh.com>
2022-07-18 23:42:16 +1000

    validation iteration 3

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
+let errors=0;
 
 function validateForm() {
     const ticketingForm = document.getElementById('ticketingForm')
     let emailValidation = /^[a-z0-9._%+-]{1,64}@[a-z0-9.-]{1,252}\.[a-z]{2,10}$/i;
     let phoneValidation = /^[0-9+ ()]{8,16}$/i;
+    let ageValidation = /^[0-9]{1,2}$/i;
 
     let onewayElement = document.querySelector('#oneway')
     let roundtripElement = document.querySelector('#roundtrip')
     let preferenceElement = document.querySelector('#preference')
     let awayIsChecked = (onewayElement.checked || roundtripElement.checked)
     validateError(awayIsChecked, preferenceElement)
-    
+
     let departureElement = document.querySelector('#departure')
     let departure = departureElement.value
     validateError(departure, departureElement)
@@ -21,11 +22,12 @@ function validateForm() {
 
     let returnElement = document.querySelector('#returnDate')
     let returnDate = returnElement.value
-    if(awayIsChecked && roundtripElement.checked) {
-        console.log('I am here')
-        validateError(returnDate, returnElement )
+    if (awayIsChecked && roundtripElement.checked) {
+        console.log(roundtripElement.checked)
+        validateError(returnDate, returnElement)
     } else {
-        validateError(false, returnElement )
+        console.log('I am there')
+        validateError(true, returnElement)
     }
 
 
@@ -35,7 +37,7 @@ function validateForm() {
 
     let passenger1Element = document.querySelector('#passenger1')
     let passenger1 = passenger1Element.value
-    validateError((passenger1.length > 5),passenger1Element )
+    validateError((passenger1.length > 5), passenger1Element)
 
 
     let male1Element = document.querySelector('#male1')
@@ -43,16 +45,24 @@ function validateForm() {
     let gender1 = document.querySelector('#gender1')
     validateError((male1Element.checked || female1Element.checked), gender1)
 
+    let age1Element = document.querySelector('#age1')
+    let age1 = age1Element.value
+    validateError(ageValidation.test(age1), age1Element)
 
     let passenger2Element = document.querySelector('#passenger2')
-    let passenger2 = passenger1Element.value
-    validateError((passenger2.length > 5),passenger2Element )
+    let passenger2 = passenger2Element.value
+    if (passenger2){
+     validateError((passenger2.length > 5), passenger2Element)
 
 
     let male2Element = document.querySelector('#male2')
     let female2Element = document.querySelector('#female2')
     let gender2 = document.querySelector('#gender2')
     validateError((male2Element.checked || female2Element.checked), gender2)
+
+    let age2Element = document.querySelector('#age2')
+    let age2 = age2Element.value
+    validateError(ageValidation.test(age2), age2Element)}
 
     let emailElement = document.querySelector('#emailAddress')
     let email = emailElement.value
@@ -61,16 +71,23 @@ function validateForm() {
     let mobileElement = document.querySelector('#mobilePhone')
     let mobile = mobileElement.value
     validateError(phoneValidation.test(mobile), mobileElement)
+
 }
 ticketingForm.addEventListener('submit', (e) => {
+    if (!errors) {
+        window.alert('your ticket request was successfully submitted')
+    }
+    errors = 0;
     e.preventDefault()
 })
 
 function validateError(result, element) {
+
     result ? valid(element) : invalid(element)
 }
 function invalid(element) {
     element.classList.add("is-invalid");
+    errors++;
 }
 function valid(element) {
     element.classList.remove("is-invalid");
